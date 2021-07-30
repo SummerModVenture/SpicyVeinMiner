b9d3a3ae054291bebb531a3d239e26eae04587da
Zach Kozar <zachkozar@vt.edu>
2021-07-30 19:14:51 -0400

    Migrated to kotlin build script

---
+++ build.gradle.kts
@@ -0,0 +1,165 @@
+import java.time.*
+import java.time.format.*
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+    id("net.minecraftforge.gradle") version "5.1.+"
+    id("net.researchgate.release") version "2.8.1"
+    `maven-publish`
+    signing
+}
+
+val archivesBaseName: String by project
+val isRelease = !version.toString().endsWith("-SNAPSHOT")
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+
+minecraft {
+    val mappingsVersion: String by project
+    mappings("official", mappingsVersion)
+
+    runs {
+        create("client") {
+            workingDirectory(project.file("run"))
+
+            property("forge.logging.markers", "REGISTRIES")
+            property("forge.logging.console.level", "DEBUG")
+
+            mods {
+                create("spicycore") {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+
+        create("server") {
+            workingDirectory(project.file("run"))
+
+            property("forge.logging.markers", "REGISTRIES")
+            property("forge.logging.console.level", "DEBUG")
+
+            mods {
+                create("spicycore") {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+
+        create("data") {
+            workingDirectory(project.file("run"))
+
+            property("forge.logging.markers", "REGISTRIES")
+            property("forge.logging.console.level", "DEBUG")
+
+            args("--mod", "examplemod", "--all", "--output", file("src/generated/resources/"), "--existing", file("src/main/resources/"))
+
+            mods {
+                create("spicycore") {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+    }
+}
+
+sourceSets.main {
+    resources.srcDir("src/generated/resources")
+}
+
+repositories {
+    maven("https://maven.masterzach32.net/artifactory/minecraft/")
+}
+
+dependencies {
+    val mcVersion: String by project
+    val forgeVersion: String by project
+    minecraft("net.minecraftforge:forge:$mcVersion-$forgeVersion")
+
+    implementation(fg.deobf("com.spicymemes:spicycore-1.17.1:2.1.1-SNAPSHOT"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "16"
+            freeCompilerArgs = listOf("-Xopt-in=kotlin.contracts.ExperimentalContracts")
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "16"
+    }
+
+    jar {
+        archiveBaseName.set(archivesBaseName)
+        manifest {
+            attributes(
+                "Specification-Title"     to "examplemod",
+                "Specification-Vendor"    to "examplemodsareus",
+                "Specification-Version"   to "1", // We are version 1 of ourselves
+                "Implementation-Title"    to project.name,
+                "Implementation-Version"  to archiveVersion,
+                "Implementation-Vendor"   to "examplemodsareus",
+                "Implementation-Timestamp" to LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            )
+        }
+
+        finalizedBy("reobfJar")
+    }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveBaseName.set(archivesBaseName)
+    archiveClassifier.set("sources")
+    from(project.sourceSets["main"].allSource)
+}
+
+tasks.assemble {
+    dependsOn(sourcesJar)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("minecraft") {
+            artifact(tasks.jar)
+            artifact(sourcesJar)
+            artifactId = archivesBaseName
+        }
+    }
+
+    repositories {
+        val mavenUsername: String? by project
+        val mavenPassword: String? by project
+        if (mavenUsername != null && mavenPassword != null) {
+            maven {
+                if (isRelease) {
+                    name = "Releases"
+                    url = uri("https://maven.masterzach32.net/artifactory/minecraft-releases/")
+                } else {
+                    name = "Snapshots"
+                    url = uri("https://maven.masterzach32.net/artifactory/minecraft-snapshots/")
+                }
+                credentials {
+                    username = mavenUsername
+                    password = mavenPassword
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["minecraft"])
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { isRelease }
+}
+
+release {
+    preTagCommitMessage = "Release version"
+    tagCommitMessage = "Release version"
+    newVersionCommitMessage = "Next development version"
+}
